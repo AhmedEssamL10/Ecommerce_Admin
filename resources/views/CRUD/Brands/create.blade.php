45f6f598cab06c21db64b5889031b4edea71d756
Ahmed Essam <201927243@std.sci.cu.edu.eg>
2023-08-24 06:00:34 +0300

    create

---
+++ resources/views/CRUD/Brands/create.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.parent')
+@section('title', 'Create Brand')
+@section('contant')
+
+    <form method="POST" enctype="multipart/form-data" action="{{ route('brands.store') }}">
+        @csrf
+        <div class="row">
+            <div iv class="col-md-6">
+                <div class="mb-3">
+                    <input type="text" name="en_name" class="form-control" placeholder="En Name"
+                        value="{{ old('en_name') }}">
+                </div>
+            </div>
+            @error('en_name')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
+            <div class="col-md-6">
+                <div class="mb-3">
+                    <input type="text" name="ar_name" class="form-control" placeholder="Ar Name"
+                        value="{{ old('ar_name') }}">
+                </div>
+            </div>
+            @error('ar_name')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
+
+            <div class="col-md-6">
+                <div class="mb-3">
+                    <label for="status">Status</label>
+                    <select name="status" id="status" class="form-control">
+                        <option @selected(old('status') == 1) value="1">Active</option>
+                        <option @selected(old('status') == 0) value="0">Not Active</option>
+
+                    </select>
+                </div>
+            </div>
+            @error('status')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
+        </div>
+
+        <div class="mb-3">
+            <input class="form-control" type="file" id="formFile" name="image" value="{{ old('image') }}">
+        </div>
+
+        <button type="submit" class="btn btn-primary">Create</button>
+    </form>
+
+@endsection
